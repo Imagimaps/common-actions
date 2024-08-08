e447432d13a2ef5650df48a54952175c629de5e8
botagar <botagar@imagimaps.com>
2024-08-08 20:33:39 +1000

    adds missing vars into s3 tf module

--- .github/actions/s3-static-site/tf/s3-bucket/variables.tf
+++ .github/actions/s3-static-site/tf/s3-bucket/variables.tf
@@ -1,3 +1,18 @@
+variable "project" {
+  description = "The name of the project"
+  type        = string
+}
+
+variable "environment" {
+  description = "The name of the environment"
+  type        = string
+}
+
+variable "aws_region" {
+  description = "The AWS region"
+  type        = string
+}
+
 variable "bucket_name" {
   description = "The name of the S3 bucket"
   type        = string
