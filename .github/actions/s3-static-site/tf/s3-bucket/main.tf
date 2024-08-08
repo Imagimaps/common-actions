9d4ee33df10bee91cc7d3ec1f7dddd9abdac6e22
botagar <botagar@imagimaps.com>
2024-08-08 20:14:06 +1000

    adds s3 static site action

---
+++ .github/actions/s3-static-site/tf/s3-bucket/main.tf
@@ -0,0 +1,71 @@
+locals {
+  account_id = data.aws_caller_identity.current.account_id
+  region     = data.aws_region.current.name
+}
+
+resource "aws_s3_bucket" "this" {
+  bucket = var.bucket_name
+  acl    = "private"
+}
+
+data "aws_iam_policy_document" "bucket_policy" {
+  statement {
+    sid       = "AdminAccess"
+    actions   = ["s3:*"]
+    resources = [
+      aws_s3_bucket.this.arn,
+      "${aws_s3_bucket.this.arn}/*"
+    ]
+    principals {
+      type        = "AWS"
+      identifiers = [
+        "arn:aws:iam::${local.account_id}:root",
+      ]
+    }
+    condition {
+      test     = "ForAllValues:StringLike"
+      variable = "aws:userid"
+      values   = [
+        "arn:aws:sts::${local.account_id}:assumed-role/aws-reserved/sso.amazonaws.com/${local.region}/AWSReservedSSO_AWSAdministratorAccess*",
+        "arn:aws:sts::${local.account_id}:assumed-role/aws-reserved/sso.amazonaws.com/${local.region}/AWSReservedSSO_AWSPowerUserAccess*",
+      ]
+    }
+  }
+
+  statement {
+    sid       = "ReadAccess"
+    actions   = [
+      "s3:Get*",
+      "s3:List*",
+    ]
+    resources = [
+      aws_s3_bucket.this.arn,
+      "${aws_s3_bucket.this.arn}/*"
+    ]
+    principals {
+      type        = "AWS"
+      identifiers = var.read_access_entities
+    }
+  }
+
+  statement {
+    sid       = "WriteAccess"
+    actions   = [
+      "s3:Put*",
+      "s3:Delete*",
+    ]
+    resources = [
+      aws_s3_bucket.this.arn,
+      "${aws_s3_bucket.this.arn}/*"
+    ]
+    principals {
+      type        = "AWS"
+      identifiers = var.write_access_entities
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "this" {
+  bucket = aws_s3_bucket.this.id
+  policy = data.aws_iam_policy_document.bucket_policy.json
+}
---
+++ .github/actions/s3-static-site/tf/s3-bucket/variables.tf
@@ -0,0 +1,28 @@
+variable "bucket_name" {
+  description = "The name of the S3 bucket"
+  type        = string
+}
+
+variable "read_access_entities" {
+  description = "The arn of entities that can read from the bucket"
+  type        = list(string)
+  default     = []
+  validation {
+    condition = alltrue([
+      for arn in var.read_access_entities : can(regex("^arn:aws:iam::[0-9]{12}:role/.+$", arn))
+    ])
+    error_message = "Each read access entity must be a valid IAM role ARN."
+  }
+}
+
+variable "write_access_entities" {
+  description = "The arn of entities that can write to the bucket"
+  type        = list(string)
+  default     = []
+  validation {
+    condition = alltrue([
+      for arn in var.read_access_entities : can(regex("^arn:aws:iam::[0-9]{12}:role/.+$", arn))
+    ])
+    error_message = "Each write access entity must be a valid IAM role ARN."
+  }
+}
