beb5489af0e35a4e73ce9f366304f317950b9e2a
botagar <botagar@imagimaps.com>
2024-08-10 15:55:38 +1000

    clean up 2

--- .github/actions/deploy-service/tf/secrets/main.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_secretsmanager_secret" "secrets" {
-  count        = length(var.secret_names)
-  name         = "${var.project}/${var.service_name}/${var.secret_names[count.index]}"
-  description  = "Secret for ${var.secret_names[count.index]}"
-}
