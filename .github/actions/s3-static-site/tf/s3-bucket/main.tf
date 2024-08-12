22e0f9ebc5b2300344551a10c708b13500d8fb83
botagar <botagar@imagimaps.com>
2024-08-12 16:29:51 +1000

    stop attempting to set a s3 bucket policy. perm issues are doing my head in

--- .github/actions/s3-static-site/tf/s3-bucket/main.tf
+++ .github/actions/s3-static-site/tf/s3-bucket/main.tf
@@ -115,7 +115,7 @@ data "aws_iam_policy_document" "bucket_policy" {
   }
 }
 
-resource "aws_s3_bucket_policy" "this" {
-  bucket = aws_s3_bucket.this.id
-  policy = data.aws_iam_policy_document.bucket_policy.json
-}
+# resource "aws_s3_bucket_policy" "this" {
+#   bucket = aws_s3_bucket.this.id
+#   policy = data.aws_iam_policy_document.bucket_policy.json
+# }
