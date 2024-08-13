8b48c29ff1329c36d86e476d79bea36684ed0937
botagar <botagar@imagimaps.com>
2024-08-13 12:34:49 +1000

    adds cf canonical user to acl

--- .github/actions/s3-static-site/tf/cloudfront/s3.tf
+++ .github/actions/s3-static-site/tf/cloudfront/s3.tf
@@ -1,3 +1,8 @@
+locals {
+  # https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/AccessLogs.html#AccessLogsBucketAndFileOwnership
+  aws_cloudfront_canonical_user_id = "c4c1ede66af53448b93c283ce9448c4ba468c9432aa01d700d3878632f77d2d0"
+}
+
 resource "aws_s3_bucket" "cloudfront_logs" {
   bucket        = "${var.fq_domain_name}-cf-logs"
   force_destroy = true
@@ -11,10 +16,30 @@ resource "aws_s3_bucket_ownership_controls" "cloudfront_logs" {
   }
 }
 
-// Set ACL for CloudFront log delivery
 resource "aws_s3_bucket_acl" "cloudfront_logs" {
   depends_on = [aws_s3_bucket_ownership_controls.cloudfront_logs]
 
   bucket = aws_s3_bucket.cloudfront_logs.id
-  acl    = "private"
+
+  access_control_policy {
+    owner {
+      id = data.aws_canonical_user_id.current.id
+    }
+
+    grant {
+      grantee {
+        id   = data.aws_canonical_user_id.current.id
+        type = "CanonicalUser"
+      }
+      permission = "FULL_CONTROL"
+    }
+
+    grant {
+      grantee {
+        id   = local.aws_cloudfront_canonical_user_id
+        type = "CanonicalUser"
+      }
+      permission = "FULL_CONTROL"
+    }
+  }
 }
